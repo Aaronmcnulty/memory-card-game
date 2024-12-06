6c860d0f7bb2280eca673fe60dacbb5c84b01ded
malcomcrab <malcomcrabesq@outlook.com>
2024-12-06 16:59:43 +0000

    Added writeup of assginments needs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,89 @@
 
 import './App.css'
 
+{/* 
+  
+  Needs:
+
+  Game Play.
+
+    Start Button 
+
+    Loading Screen
+
+    Store name and image data for selected cards in the round
+
+    Store and update current Score Tally
+
+    Store and update best Score 
+
+    Checks for Losing move
+
+    Checks for user win
+
+    Score Board
+
+    Reset Button
+
+    Reset on loss or win.
+
+
+  1. Card Creation.
+
+    - Fetch API and randomly select 12 items.
+
+    - Store image links and name.
+
+    - Template each cards visual features and add functions:
+        
+      1. Card needs to start in an 'unclicked' state.
+      2. Must register and store when it is clicked.
+
+    - Pass 8 selected items through card templates to create visual display.
+  
+  2. Card Display component.
+
+    - Display the cards in random order in the browser.
+
+    - Once a card is clicked the display order must rearange. 
+
+  3. Score Board.
+
+      - Logs total number of cards clicked
+
+      - Updates when cards state changes to clicked
+
+      - Passes the data to be stored
+
+      - Resets when game is lost
+
+      - Displays best score of all rounds
+    
+    4. Reset/Restart.
+      
+      - Restarts card creations process
+
+      - Removes stored click state
+
+      - Remove Current score
+
+
+
+
+
+
+
+  */}
+
+
+
+
 function App() {
+
+
+
+
+
 
 
   return (
