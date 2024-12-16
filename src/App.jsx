460e7809712d4c21ac6b1215a377a2acd17a3fbf
malcomcrab <malcomcrabesq@outlook.com>
2024-12-16 12:07:18 +0000

    CSS formatting of the cards display

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { getRandomInt } from './modules/randomInt'
 import GameDisplay from './components/gameDisplay/GameDisplay'
 import ScoreBoard from './components/scoreBoard/ScoreBoard'
 
-
+import { shuffle } from './modules/shuffle'
 
 function App() {
 // https://pokeapi.co/api/v2/pokemon/ditto
@@ -63,24 +63,7 @@ const handleCardClick = (event) => {
 }
 
 
-//Shuffles the array which changes the order the cards are displayed in.
 
-function shuffle(array) {
-  let currentIndex = array.length;
-
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-
-    // Pick a remaining element...
-    let randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-  return array
-}
 
   return (
     <>
--- src/components/cardDisplay/CardTemplate.jsx
+++ src/components/cardDisplay/CardTemplate.jsx
@@ -5,7 +5,7 @@ function CardTemplate({ cardData, clickedCards, setClickedCards, handleCardClick
 return(
     
     <div className="pokemon-card" >
-        <img id={cardData.name} onClick={handleCardClick} className="pokemon-card-image" src={cardData.sprites.other.dream_world.front_default} width={"150px"} height={'150px'} ></img>
+        <img id={cardData.name} onClick={handleCardClick} className="pokemon-card-image" src={cardData.sprites.other.dream_world.front_default}  ></img>
         <h3 className="pokemon-card-title">{cardData.name}</h3>
     </div>
 
